baa2374e5058ba79c389e6def62ae883f8dcd212
dustinturp <dustinturp@outlook.com>
2021-11-06 11:23:26 -0500

    feat: worked on associations

--- models/Tag.js
+++ models/Tag.js
@@ -6,7 +6,8 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    id: {DataTypes.INTEGER,
+    id: {
+    type:DataTypes.INTEGER,
     allowNull: false,
     primaryKey: true,
     autoIncrement: true,
