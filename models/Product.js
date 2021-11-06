62973deabebb8ebe4c598aa492d72480798c1b72
HmndTyphn <Jared.elliott09@gmail.com>
2021-11-06 10:56:32 -0500

    feat: finished product and tag attributes

--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,7 @@
 const { Model, DataTypes, DECIMAL } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const Category = require('./Category');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -30,6 +31,13 @@ Product.init(
         type: DataTypes.INTEGER,
         allowNull: false,
         default: 10
+      },
+      category_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'category',
+          key: 'id'
+        }
       }
   },
   {
--- models/Tag.js
+++ models/Tag.js
@@ -6,7 +6,14 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    id: {DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    }
   },
   {
     sequelize,
