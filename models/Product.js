194d89c3cf81f3f7ec5d8e055666c7461689ba15
DarrickPrichett <darrickprichett@gmail.com>
2021-11-06 10:35:43 -0500

    feat: added model definition needs more work

--- models/Product.js
+++ models/Product.js
@@ -9,14 +9,25 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    {
+      product_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    }
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: 'product'
   }
 );
 
